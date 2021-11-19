b7a5180e77b83b5edb394908965b4ba14a407c70
VolletMaxence <70692732+VolletMaxence@users.noreply.github.com>
2021-11-19 15:36:28 +0100

    Affichage QLineEdit, ne detect pas la touche entre

--- Historiqu/Historiqu/Historiqu.cpp
+++ Historiqu/Historiqu/Historiqu.cpp
@@ -1,8 +1,15 @@
 #include "Historiqu.h"
+#include "QLineEdit_Histo.h"
 #include <QLineEdit>
 
 Historiqu::Historiqu(QWidget *parent)
     : QMainWindow(parent)
 {
+	QLineEdit_Histo QLineEdit_Histo;
+
     ui.setupUi(this);
+	QLineEdit *Texte = new QLineEdit;
+	Texte->move(15, 50);
+	Texte->resize(100, 20);
+	Texte->setParent(this);
 }
--- Historiqu/Historiqu/QLineEdit_Histo.cpp
+++ Historiqu/Historiqu/QLineEdit_Histo.cpp
@@ -5,10 +5,15 @@ QLineEdit_Histo::QLineEdit_Histo(int TailleMax)
 {
 	this->tailleHisto = TailleMax;
 
-	QObject::connect(this, SIGNAL(returnPressed()) , this, SLOT(insertHisto));
+	QObject::connect(this, SIGNAL(returnPressed()) , this, SLOT(insertHisto()));
 }
 
 void QLineEdit_Histo::insertHisto()
 {
 	qDebug() << "App";
+	//Envoyer ce qui a été ecrit
+
+	//Vider ce qui a été écrit
+	this->labelMdP->clear();
+
 }
